dc9d58daa26b3937e5e849fcf85f8804caca071e
Jorel Magatti <jorel.magatti@bussoladoinvestidor.com>
2021-10-13 22:57:00 -0300

    estrutura inicial do projeto

---
+++ GenerateUrlPutSolution/API.GenerateUrlPut/Controllers/GenerateUrlUploadController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Services.GenerateUrlPut.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.GenerateUrlPut.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenerateUrlUploadController : ControllerBase
+    {
+        private readonly IGenerateUrlAWSs3Service _IGenerateUrlAWSs3Service;
+
+        public GenerateUrlUploadController(IGenerateUrlAWSs3Service generateUrlAWSs3Service)
+        => _IGenerateUrlAWSs3Service = generateUrlAWSs3Service;
+
+        [HttpGet]
+        [Route("v1/GetNewUrlForUpload/{namefile}")]
+        public IActionResult GetNewUrlForUpload(string namefile)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ GenerateUrlPutSolution/Service.GenerateUrlPut/Services/GenerateUrlAWSs3Service.cs
@@ -0,0 +1,14 @@
+﻿using Services.GenerateUrlPut.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.GenerateUrlPut.Service
+{
+    public class GenerateUrlAWSs3Service : IGenerateUrlAWSs3Service
+    {
+
+    }
+}
